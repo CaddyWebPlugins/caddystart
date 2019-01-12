cf9fa23cf3b1834a912ab5f18ab4cdb5c16bb03c
SpivEgin <spivegin@txtsme.com>
2019-01-12 13:00:32 -0500

    set app branding

--- build.cmd
+++ build.cmd
@@ -2,3 +2,6 @@ set DEBUG=true
 set GOOS=linux
 set EMAIL=ssl@exaple.com
 go build -o caddystart main.go
+
+cp caddystart /c/Source/docker/CockroachDockerCluster/bin/
+copy caddystart C:\Source\golang\src\sc.tpnfc.us\oyoshi\cockroachcluster\DockerFiles\docker\caddy\
